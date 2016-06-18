3bef239261fa9f1113c4c4f86243c75d4c895b1b
Pekar Andrej <andrej.pekar@everlution.sk>
2016-06-18 20:51:08 +0200

    init colorpicker

---
+++ Colorpicker/ColorPickerForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Colorpicker
+{
+    partial class ColorPickerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ColorPickerForm));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pickerColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.optionsToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pickerColorToolStripMenuItem,
+            this.topToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // pickerColorToolStripMenuItem
+            // 
+            this.pickerColorToolStripMenuItem.Name = "pickerColorToolStripMenuItem";
+            this.pickerColorToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pickerColorToolStripMenuItem.Text = "Picker color";
+            this.pickerColorToolStripMenuItem.Click += new System.EventHandler(this.pickerColorToolStripMenuItem_Click);
+            // 
+            // topToolStripMenuItem
+            // 
+            this.topToolStripMenuItem.Name = "topToolStripMenuItem";
+            this.topToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.topToolStripMenuItem.Text = "Top";
+            this.topToolStripMenuItem.Click += new System.EventHandler(this.topToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ColorPickerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(100, 100);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ColorPickerForm";
+            this.Text = "ColorPicker";
+            this.Activated += new System.EventHandler(this.ColorPickerForm_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ColorPickerForm_FormClosing);
+            this.Load += new System.EventHandler(this.ColorPickerForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ColorPickerForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ColorPickerForm_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ColorPickerForm_MouseDown);
+            this.Move += new System.EventHandler(this.ColorPickerForm_Move);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pickerColorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem topToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Colorpicker/ColorPickerForm.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colorpicker
+{
+    
+    public partial class ColorPickerForm : Form
+    {
+        public ColorPickerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ColorPickerForm_Load(object sender, EventArgs e)
+        {
+            this.Left = Properties.Settings.Default.PosLeft;
+            this.Top = Properties.Settings.Default.PosTop;
+            this.selector = System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.Selector);
+            this.TopMost = Properties.Settings.Default.TopMost;
+
+            this.ShowInTaskbar = false;
+            this.ShowInTaskbar = true;
+
+            this.Height = 80;
+            this.Width = 100;
+
+            this.TransparencyKey = Color.LimeGreen;
+            this.selector = Color.Red;
+
+            
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void ColorPickerForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                Point pt = this.PointToScreen(e.Location);
+                contextMenuStrip1.Show(pt);
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+
+        public Color GetColorAt(int x, int y)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            int a = (int)GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+            return Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
+        }
+
+        private void ColorPickerForm_Move(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        Color color;
+        Color selector;
+
+        private void ColorPickerForm_Paint(object sender, PaintEventArgs e)
+        {
+            int t = this.Top;
+            int l = this.Left;
+            int w = this.Width;
+
+
+            // top space
+            System.Drawing.SolidBrush myBrushTop = new System.Drawing.SolidBrush(
+                this.TransparencyKey
+            );
+            e.Graphics.FillRectangle(myBrushTop, new Rectangle(0, 0, 100, 50));
+
+            //botom space
+            this.color = GetColorAt(l+50, t-1);
+            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(
+                 this.color
+            );
+            e.Graphics.FillRectangle(myBrush, new Rectangle(0, 50, 100, 30));
+
+
+            System.Drawing.Pen myPen = new System.Drawing.Pen(this.selector);
+            e.Graphics.DrawLine(myPen, 50, 0, 50, 50);
+            e.Graphics.DrawLine(myPen, 0, 50, 100, 50);
+            e.Graphics.DrawLine(myPen, 0, 50, 0, 80);
+            e.Graphics.DrawLine(myPen, 0, 79, 30, 79);
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(
+                System.Drawing.ColorTranslator.ToHtml(this.color)
+            );
+        }
+
+        private void ColorPickerForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F10 || e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+
+
+            int add = 1;
+            if (e.Alt)
+                add = 20;
+
+            if (e.Control || (e.Control && e.Alt))
+            {
+                if (e.KeyCode == Keys.Left)
+                {
+                    this.Width = this.Width - add;
+                }
+                if (e.KeyCode == Keys.Right)
+                {
+                    this.Width = this.Width + add;
+                }
+                if (e.KeyCode == Keys.Up)
+                {
+                    this.Height = this.Height - add;
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    this.Height = this.Height + add;
+                }
+            }
+            else
+            {
+                if (e.KeyCode == Keys.Left)
+                {
+                    this.Left = this.Left - add;
+                }
+                if (e.KeyCode == Keys.Right)
+                {
+                    this.Left = this.Left + add;
+                }
+                if (e.KeyCode == Keys.Up)
+                {
+                    this.Top = this.Top - add;
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    this.Top = this.Top + add;
+                }
+            }
+        }
+
+        private void pickerColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.selector = colorDialog1.Color;
+
+                if (this.TransparencyKey == this.selector) {
+                    this.TransparencyKey = Color.FromArgb(this.selector.ToArgb() ^ 0xffffff);
+                }
+
+                this.Invalidate();
+            }
+        }
+
+        private void ColorPickerForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.PosLeft = this.Left;
+            Properties.Settings.Default.PosTop = this.Top;
+            Properties.Settings.Default.Selector = System.Drawing.ColorTranslator.ToHtml(this.selector);
+            Properties.Settings.Default.TopMost = this.TopMost;
+            Properties.Settings.Default.Save();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            this.copyToolStripMenuItem.Text = System.Drawing.ColorTranslator.ToHtml(this.color);
+            topToolStripMenuItem.Checked = this.TopMost;
+        }
+
+        private void topToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
+            topToolStripMenuItem.Checked = this.TopMost;
+        }
+
+        private void ColorPickerForm_Activated(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.color != GetColorAt(this.Left + 50, this.Top - 1)) {
+                this.Invalidate();
+            }
+        }
+    }
+}
